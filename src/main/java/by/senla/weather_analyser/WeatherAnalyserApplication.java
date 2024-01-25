318d01a8cdd58379924e0cb2cfc2847039df6c20
Hanna Saurytskaya <anna.kassy.s@gmail.com>
2024-01-25 11:29:35 +0300

    Missing annotation added.

--- src/main/java/by/senla/weather_analyser/WeatherAnalyserApplication.java
+++ src/main/java/by/senla/weather_analyser/WeatherAnalyserApplication.java
@@ -4,11 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @EnableJpaRepositories
 @EnableTransactionManagement
 @EnableFeignClients
+@EnableScheduling
 @SpringBootApplication
 public class WeatherAnalyserApplication {
 
